96c69966c28a9a85b5d00968229044789eb14cac
Phạm Lộc <tomiie6879@gmail.com>
2026-07-01 20:52:44 +0700

    Update

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -10,3 +10,24 @@ ct.Sound();
 Duck dd = new Duck();
 dd.Eat("Duck");
 dd.Sound();
+
+// bai 2 
+Car cc = new Car();
+cc.Start("Porsche");
+cc.Move(30);
+cc.Stop();
+Boat boat = new Boat();
+boat.Start("Boat");
+boat.Move(20);
+boat.Stop();
+Bike bk = new Bike();
+bk.Start("Bike");
+bk.Move(10);
+bk.Stop();
+// bai 3
+Bird bd = new Bird();
+bd.Fly();
+Airplane a = new Airplane();
+a.Fly();
+SuperMan sm = new SuperMan();
+sm.Fly();
